c16f0518ebbe97e49528791231621f7016e6ca2c
Konstantin Hintermayer <hintermayer.konstantin@gmail.com>
2022-08-23 21:25:17 +0200

    Fixed the Inkwell widget and setState Logic

--- lib/presentation/home/components/chair_selection.dart
+++ lib/presentation/home/components/chair_selection.dart
@@ -10,6 +10,8 @@ class ChairSelect extends StatefulWidget {
 }
 
 class _ChairSelectState extends State<ChairSelect> {
+  var chair = List.filled(9, false);
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -23,44 +25,53 @@ class _ChairSelectState extends State<ChairSelect> {
         centerWidget: ClipOval(
           child: Image.asset('assets/images/table.png'),
         ),
-        children: List.generate((chairSlot.length), (index) {
-          return InkWell(
+        children: List.generate(
+          (chairSlot.length),
+          (index) {
+            return InkWell(
+              borderRadius: (index % 10 == 9
+                        ? const BorderRadius.all(Radius.circular(30))
+                        : const BorderRadius.all(Radius.circular(90))),
               onTap: (() {
                 setState(() {
-                  var chair = chairSlot.any((element) => false);
-                  chair = _chair;
+                  chair[index] = !chair[index];
                 });
               }),
               child: Card(
-                  color: (index % 10 == 9 ? Colors.black : Colors.white54),
-                  shape: RoundedRectangleBorder(
-                      side: const BorderSide(color: Colors.white24),
-                      borderRadius: (index % 10 == 9
-                          ? const BorderRadius.all(Radius.circular(30))
-                          : const BorderRadius.all(Radius.circular(90)))),
-                  //margin: const EdgeInsets.symmetric(vertical: 30),
-                  child: GridTile(
-                    child: Center(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(chairSlot.elementAt(index),
-                                style: const TextStyle(color: Colors.red)),
-                            const Text('FREI'),
-                            Container(
-                                child: (index % 10 == 9
-                                    ? null
-                                    : Icon(
-                                        Icons.chair_outlined,
-                                        color:
-                                            _chair ? Colors.grey : Colors.red,
-                                        size: 30,
-                                      )))
-                          ]),
+                color: (index % 10 == 9 ? Colors.black : Colors.white54),
+                shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: Colors.white24),
+                    borderRadius: (index % 10 == 9
+                        ? const BorderRadius.all(Radius.circular(30))
+                        : const BorderRadius.all(Radius.circular(90)))),
+                //margin: const EdgeInsets.symmetric(vertical: 30),
+                child: GridTile(
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(chairSlot.elementAt(index),
+                            style: const TextStyle(color: Colors.red)),
+                        const Text('FREI'),
+                        Container(
+                          child: (index % 10 == 9
+                              ? null
+                              : Icon(
+                                  Icons.chair_outlined,
+                                  color:
+                                      chair[index] ? Colors.grey : Colors.red,
+                                  size: 30,
+                                )),
+                        ),
+                      ],
                     ),
-                  )));
-        }),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
